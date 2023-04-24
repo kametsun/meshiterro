850c7239f844b63be1738460d5dda6a81f064576
EC2 Default User <ec2-user@ip-172-31-29-153.ap-northeast-1.compute.internal>
2023-04-24 13:43:02 +0000

    一旦保存

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,38 @@
+class PostImagesController < ApplicationController
+
+  def new
+    @post_image = PostImage.new
+  end
+
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
+
+  def index
+    @post_images = PostImage.all
+  end
+
+  def show
+    @post_image = PostImage.find(params[:id])
+  end
+
+  def destroy
+    #削除するPostImageレコードを取得
+    post_image = PostImage.find(params[:id])
+    # データ（レコード）を削除
+    post_image.destroy
+    #PostImageの一覧ページへ
+    redirect_to post_images_path
+  end
+
+  #投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
+end
